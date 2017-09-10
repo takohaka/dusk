1d1a2a0ae34f0884fd9348381101ea754021879a
takohaka <takohaka@gmail.com>
2017-09-10 17:00:53 +0900

    失敗

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ワーイ
+                    失敗
                 </div>
             </div>
         </div>
