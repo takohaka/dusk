cb4a975210d1a51e703ec3b8f4baa70be404105f
takohaka <takohaka@gmail.com>
2017-09-10 16:50:02 +0900

    ワーイ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ワーイ
                 </div>
 
                 <div class="links">
