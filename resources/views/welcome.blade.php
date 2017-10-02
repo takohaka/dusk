e61290ca1f7811eca5352738e34102be107ec5e5
takohaka <takohaka@gmail.com>
2017-10-02 11:44:31 +0900

    成功

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    失敗
+                    ワーイ
                 </div>
             </div>
         </div>
